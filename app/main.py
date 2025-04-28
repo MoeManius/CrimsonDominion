c1eaeafec65944ae256a09624a1d1e923777985c
MoeManius <moe.yassir1992@gmail.com>
2025-04-28 22:54:19 +0200

    Deployment Update
    Minor changes
    removed CORS
    Type added to buildings
    /me endpoint fix
    Removed redundant battle router

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ from users.endpoints import router as users_router
 from buildings.endpoints import router as buildings_router
 from user_buildings.endpoints import router as user_buildings_router
 from user_fleets.endpoints import router as user_fleets_router
-from battles.endpoints import router as battles_router
-from planets.endpoints import router as planets_router
 from user_battles.endpoints import router as battle_router
+from planets.endpoints import router as planets_router
+
 # -----------------------
 # Logging Setup
 # -----------------------
@@ -43,17 +43,6 @@ Use this API to manage players, battles, planets, buildings, and fleets.
 )
 
 # -----------------------
-# CORS Middleware
-# -----------------------
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # In production: specify trusted frontend URLs
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# -----------------------
 # Include All Routers
 # -----------------------
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
@@ -61,8 +50,7 @@ app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(buildings_router, prefix="/buildings", tags=["Buildings"])
 app.include_router(user_buildings_router, prefix="/user-buildings", tags=["User Buildings"])
 app.include_router(user_fleets_router, prefix="/user-fleets", tags=["User Fleets"])
-app.include_router(battles_router, prefix="/battles", tags=["Battles"])
-app.include_router(battle_router, prefix="/battles", tags=["battles"])
+app.include_router(battle_router, prefix="/user-battles", tags=["User Battles"])  # Corrected prefix
 app.include_router(planets_router, prefix="/planets", tags=["Planets"])
 
 # -----------------------
